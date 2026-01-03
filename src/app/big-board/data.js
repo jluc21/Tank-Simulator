891b042dd87cb024e518989956b806479450c9af
jluc21 <164274427+jluc21@users.noreply.github.com>
2026-01-03 12:42:05 -0800

    Update data.js

--- src/app/big-board/data.js
+++ src/app/big-board/data.js
@@ -1,1 +1,121 @@
+// src/app/big-board/data.js
 
+export const prospects = [
+  { 
+    id: 1, rank: 1, name: "Darryn Peterson", school: "Kansas", position: "G", 
+    stats: { ppg: "19.3", rpg: "3.8", apg: "2.8", fg: "52.8%", threePt: "42.3%" },
+    img: "https://a.espncdn.com/i/teamlogos/ncaa/500/2305.png" 
+  },
+  { 
+    id: 2, rank: 2, name: "Cameron Boozer", school: "Duke", position: "PF", 
+    stats: { ppg: "22.1", rpg: "11.7", apg: "3.2", fg: "56.5%", threePt: "38.5%" },
+    img: "https://a.espncdn.com/i/teamlogos/ncaa/500/150.png" 
+  },
+  { 
+    id: 3, rank: 3, name: "AJ Dybantsa", school: "BYU", position: "SF", 
+    stats: { ppg: "23.1", rpg: "7.2", apg: "3.8", fg: "59.1%", threePt: "33.3%" },
+    img: "https://a.espncdn.com/i/teamlogos/ncaa/500/252.png" 
+  },
+  { 
+    id: 4, rank: 4, name: "Caleb Wilson", school: "UNC", position: "PF", 
+    stats: { ppg: "21.7", rpg: "11.0", apg: "5.0", fg: "54.0%", threePt: "36.0%" },
+    img: "https://a.espncdn.com/i/teamlogos/ncaa/500/153.png" 
+  },
+  { 
+    id: 5, rank: 5, name: "Nate Ament", school: "Tennessee", position: "SF", 
+    stats: { ppg: "18.9", rpg: "10.0", apg: "2.2", fg: "48.5%", threePt: "41.2%" },
+    img: "https://a.espncdn.com/i/teamlogos/ncaa/500/2633.png" 
+  },
+  { 
+    id: 6, rank: 6, name: "Kingston Flemings", school: "Houston", position: "PG", 
+    stats: { ppg: "20.4", rpg: "6.4", apg: "6.8", fg: "46.0%", threePt: "35.0%" },
+    img: "https://a.espncdn.com/i/teamlogos/ncaa/500/248.png" 
+  },
+  { 
+    id: 7, rank: 7, name: "Mikel Brown Jr.", school: "Louisville", position: "PG", 
+    stats: { ppg: "14.9", rpg: "3.3", apg: "6.1", fg: "44.0%", threePt: "38.0%" },
+    img: "https://a.espncdn.com/i/teamlogos/ncaa/500/97.png" 
+  },
+  { 
+    id: 8, rank: 8, name: "Braylon Mullins", school: "UConn", position: "SG", 
+    stats: { ppg: "17.0", rpg: "5.4", apg: "2.1", fg: "50.0%", threePt: "47.6%" },
+    img: "https://a.espncdn.com/i/teamlogos/ncaa/500/41.png" 
+  },
+  { 
+    id: 9, rank: 9, name: "Koa Peat", school: "Arizona", position: "PF", 
+    stats: { ppg: "18.0", rpg: "10.0", apg: "4.9", fg: "56.0%", threePt: "28.0%" },
+    img: "https://a.espncdn.com/i/teamlogos/ncaa/500/12.png" 
+  },
+  { 
+    id: 10, rank: 10, name: "Tounde Yessoufou", school: "Baylor", position: "SF", 
+    stats: { ppg: "23.7", rpg: "7.2", apg: "2.7", fg: "49.0%", threePt: "36.0%" },
+    img: "https://a.espncdn.com/i/teamlogos/ncaa/500/239.png" 
+  },
+  { 
+    id: 11, rank: 11, name: "Jayden Quaintance", school: "Arizona St", position: "C", 
+    stats: { ppg: "11.2", rpg: "14.4", apg: "1.2", fg: "60.0%", threePt: "0.0%" },
+    img: "https://a.espncdn.com/i/teamlogos/ncaa/500/9.png" 
+  },
+  { 
+    id: 12, rank: 12, name: "Labaron Philon", school: "Alabama", position: "PG", 
+    stats: { ppg: "15.5", rpg: "4.5", apg: "7.0", fg: "48.0%", threePt: "39.0%" },
+    img: "https://a.espncdn.com/i/teamlogos/ncaa/500/333.png" 
+  },
+  { 
+    id: 13, rank: 13, name: "Darius Acuff", school: "Arkansas", position: "PG", 
+    stats: { ppg: "16.2", rpg: "3.2", apg: "4.5", fg: "42.0%", threePt: "34.0%" },
+    img: "https://a.espncdn.com/i/teamlogos/ncaa/500/8.png" 
+  },
+  { 
+    id: 14, rank: 14, name: "Yaxel Lendeborg", school: "Michigan", position: "PF", 
+    stats: { ppg: "21.0", rpg: "9.6", apg: "4.8", fg: "52.0%", threePt: "38.0%" },
+    img: "https://a.espncdn.com/i/teamlogos/ncaa/500/130.png" 
+  },
+  { id: 15, rank: 15, name: "Chris Cenac", school: "Houston", position: "C", stats: { ppg: "12.4", rpg: "8.5", apg: "1.2", fg: "62%", threePt: "0%" }, img: "https://a.espncdn.com/i/teamlogos/ncaa/500/248.png" },
+  { id: 16, rank: 16, name: "Meleek Thomas", school: "Arkansas", position: "SG", stats: { ppg: "15.8", rpg: "4.2", apg: "3.1", fg: "45%", threePt: "37%" }, img: "https://a.espncdn.com/i/teamlogos/ncaa/500/8.png" },
+  { id: 17, rank: 17, name: "Jalen Haralson", school: "Notre Dame", position: "SF", stats: { ppg: "14.2", rpg: "5.5", apg: "4.0", fg: "48%", threePt: "34%" }, img: "https://a.espncdn.com/i/teamlogos/ncaa/500/87.png" },
+  { id: 18, rank: 18, name: "Bryson Tiller", school: "Kansas", position: "PF", stats: { ppg: "13.5", rpg: "7.8", apg: "1.5", fg: "55%", threePt: "30%" }, img: "https://a.espncdn.com/i/teamlogos/ncaa/500/2305.png" },
+  { id: 19, rank: 19, name: "Jasper Johnson", school: "Kentucky", position: "G", stats: { ppg: "16.1", rpg: "2.8", apg: "3.5", fg: "44%", threePt: "39%" }, img: "https://a.espncdn.com/i/teamlogos/ncaa/500/96.png" },
+  { id: 20, rank: 20, name: "Malachi Moreno", school: "Kentucky", position: "C", stats: { ppg: "9.8", rpg: "9.2", apg: "1.0", fg: "68%", threePt: "0%" }, img: "https://a.espncdn.com/i/teamlogos/ncaa/500/96.png" },
+  { id: 21, rank: 21, name: "Eric Reibe", school: "UConn", position: "C", stats: { ppg: "10.5", rpg: "8.0", apg: "2.2", fg: "58%", threePt: "33%" }, img: "https://a.espncdn.com/i/teamlogos/ncaa/500/41.png" },
+  { id: 22, rank: 22, name: "Nikolas Khamenia", school: "Gonzaga", position: "SF", stats: { ppg: "12.2", rpg: "5.1", apg: "3.4", fg: "49%", threePt: "38%" }, img: "https://a.espncdn.com/i/teamlogos/ncaa/500/2250.png" },
+  { id: 23, rank: 23, name: "Acaden Lewis", school: "Kentucky", position: "PG", stats: { ppg: "14.5", rpg: "3.0", apg: "5.5", fg: "43%", threePt: "36%" }, img: "https://a.espncdn.com/i/teamlogos/ncaa/500/96.png" },
+  { id: 24, rank: 24, name: "Karim Lopez", school: "N. Zealand", position: "SF", stats: { ppg: "15.4", rpg: "6.5", apg: "2.1", fg: "47%", threePt: "35%" }, img: "https://a.espncdn.com/i/teamlogos/ncaa/500/2483.png" },
+  { id: 25, rank: 25, name: "Hugo Gonzalez", school: "R. Madrid", position: "SF", stats: { ppg: "11.2", rpg: "4.0", apg: "1.8", fg: "50%", threePt: "32%" }, img: "https://upload.wikimedia.org/wikipedia/en/thumb/a/a6/Real_Madrid_CF_logo.svg/1200px-Real_Madrid_CF_logo.svg.png" },
+  { id: 26, rank: 26, name: "Will Riley", school: "Illinois", position: "SF", stats: { ppg: "17.5", rpg: "5.5", apg: "2.5", fg: "46%", threePt: "41%" }, img: "https://a.espncdn.com/i/teamlogos/ncaa/500/356.png" },
+  { id: 27, rank: 27, name: "Morez Johnson", school: "Illinois", position: "C", stats: { ppg: "11.0", rpg: "9.5", apg: "0.8", fg: "63%", threePt: "0%" }, img: "https://a.espncdn.com/i/teamlogos/ncaa/500/356.png" },
+  { id: 28, rank: 28, name: "Tre Johnson", school: "Texas", position: "SG", stats: { ppg: "18.2", rpg: "4.5", apg: "2.5", fg: "45%", threePt: "38%" }, img: "https://a.espncdn.com/i/teamlogos/ncaa/500/251.png" },
+  { id: 29, rank: 29, name: "Jalil Bethea", school: "Miami", position: "SG", stats: { ppg: "16.8", rpg: "3.2", apg: "2.0", fg: "44%", threePt: "40%" }, img: "https://a.espncdn.com/i/teamlogos/ncaa/500/2390.png" },
+  { id: 30, rank: 30, name: "Ian Jackson", school: "UNC", position: "SG", stats: { ppg: "15.5", rpg: "4.0", apg: "2.8", fg: "47%", threePt: "35%" }, img: "https://a.espncdn.com/i/teamlogos/ncaa/500/153.png" },
+  { id: 31, rank: 31, name: "Drake Powell", school: "UNC", position: "SF", stats: { ppg: "12.4", rpg: "6.0", apg: "3.2", fg: "51%", threePt: "34%" }, img: "https://a.espncdn.com/i/teamlogos/ncaa/500/153.png" },
+  { id: 32, rank: 32, name: "Derrion Reid", school: "Alabama", position: "SF", stats: { ppg: "13.0", rpg: "5.5", apg: "2.0", fg: "49%", threePt: "36%" }, img: "https://a.espncdn.com/i/teamlogos/ncaa/500/333.png" },
+  { id: 33, rank: 33, name: "Aiden Sherrell", school: "Alabama", position: "C", stats: { ppg: "10.2", rpg: "7.5", apg: "0.9", fg: "60%", threePt: "28%" }, img: "https://a.espncdn.com/i/teamlogos/ncaa/500/333.png" },
+  { id: 34, rank: 34, name: "Asa Newell", school: "Georgia", position: "PF", stats: { ppg: "14.1", rpg: "8.2", apg: "1.1", fg: "54%", threePt: "32%" }, img: "https://a.espncdn.com/i/teamlogos/ncaa/500/61.png" },
+  { id: 35, rank: 35, name: "Khani Rooths", school: "Michigan", position: "PF", stats: { ppg: "11.5", rpg: "6.0", apg: "1.5", fg: "50%", threePt: "31%" }, img: "https://a.espncdn.com/i/teamlogos/ncaa/500/130.png" },
+  { id: 36, rank: 36, name: "Donnie Freeman", school: "Syracuse", position: "PF", stats: { ppg: "13.8", rpg: "7.8", apg: "1.2", fg: "52%", threePt: "30%" }, img: "https://a.espncdn.com/i/teamlogos/ncaa/500/183.png" },
+  { id: 37, rank: 37, name: "Kon Knueppel", school: "Duke", position: "SF", stats: { ppg: "14.5", rpg: "4.5", apg: "2.5", fg: "48%", threePt: "42%" }, img: "https://a.espncdn.com/i/teamlogos/ncaa/500/150.png" },
+  { id: 38, rank: 38, name: "Cooper Koch", school: "Iowa", position: "PF", stats: { ppg: "12.2", rpg: "5.0", apg: "1.8", fg: "47%", threePt: "40%" }, img: "https://a.espncdn.com/i/teamlogos/ncaa/500/2294.png" },
+  { id: 39, rank: 39, name: "Jackson McAndrew", school: "Creighton", position: "PF", stats: { ppg: "11.8", rpg: "5.2", apg: "1.0", fg: "46%", threePt: "41%" }, img: "https://a.espncdn.com/i/teamlogos/ncaa/500/156.png" },
+  { id: 40, rank: 40, name: "Jase Richardson", school: "Mich St", position: "PG", stats: { ppg: "10.5", rpg: "3.5", apg: "4.2", fg: "44%", threePt: "35%" }, img: "https://a.espncdn.com/i/teamlogos/ncaa/500/127.png" },
+  { id: 41, rank: 41, name: "Kur Teng", school: "Mich St", position: "SG", stats: { ppg: "12.1", rpg: "3.0", apg: "1.5", fg: "45%", threePt: "39%" }, img: "https://a.espncdn.com/i/teamlogos/ncaa/500/127.png" },
+  { id: 42, rank: 42, name: "Annor Boateng", school: "Missouri", position: "SF", stats: { ppg: "11.4", rpg: "4.8", apg: "1.8", fg: "48%", threePt: "33%" }, img: "https://a.espncdn.com/i/teamlogos/ncaa/500/142.png" },
+  { id: 43, rank: 43, name: "Rakease Passmore", school: "Kansas", position: "SG", stats: { ppg: "9.8", rpg: "3.2", apg: "1.2", fg: "46%", threePt: "37%" }, img: "https://a.espncdn.com/i/teamlogos/ncaa/500/2305.png" },
+  { id: 44, rank: 44, name: "Carter Bryant", school: "Arizona", position: "SF", stats: { ppg: "10.5", rpg: "5.5", apg: "2.0", fg: "47%", threePt: "34%" }, img: "https://a.espncdn.com/i/teamlogos/ncaa/500/12.png" },
+  { id: 45, rank: 45, name: "Jamari Phillips", school: "Arizona", position: "SG", stats: { ppg: "11.2", rpg: "2.5", apg: "1.8", fg: "43%", threePt: "38%" }, img: "https://a.espncdn.com/i/teamlogos/ncaa/500/12.png" },
+  { id: 46, rank: 46, name: "Zoom Diallo", school: "Washington", position: "PG", stats: { ppg: "13.5", rpg: "4.0", apg: "5.2", fg: "44%", threePt: "30%" }, img: "https://a.espncdn.com/i/teamlogos/ncaa/500/264.png" },
+  { id: 47, rank: 47, name: "Tahaad Pettiford", school: "Auburn", position: "PG", stats: { ppg: "12.8", rpg: "2.8", apg: "4.5", fg: "42%", threePt: "35%" }, img: "https://a.espncdn.com/i/teamlogos/ncaa/500/2.png" },
+  { id: 48, rank: 48, name: "Jahki Howard", school: "Auburn", position: "SF", stats: { ppg: "10.1", rpg: "4.2", apg: "1.1", fg: "51%", threePt: "32%" }, img: "https://a.espncdn.com/i/teamlogos/ncaa/500/2.png" },
+  { id: 49, rank: 49, name: "Vyctorius Miller", school: "LSU", position: "SG", stats: { ppg: "13.2", rpg: "3.5", apg: "2.2", fg: "45%", threePt: "36%" }, img: "https://a.espncdn.com/i/teamlogos/ncaa/500/99.png" },
+  { id: 50, rank: 50, name: "Robert Wright III", school: "Baylor", position: "PG", stats: { ppg: "11.5", rpg: "2.5", apg: "5.8", fg: "43%", threePt: "34%" }, img: "https://a.espncdn.com/i/teamlogos/ncaa/500/239.png" },
+  { id: 51, rank: 51, name: "Dink Pate", school: "G League", position: "SG", stats: { ppg: "14.5", rpg: "5.2", apg: "3.8", fg: "41%", threePt: "31%" }, img: "https://a.espncdn.com/i/teamlogos/ncaa/500/264.png" },
+  { id: 52, rank: 52, name: "Joson Sanon", school: "Arizona St", position: "SG", stats: { ppg: "15.2", rpg: "3.8", apg: "2.1", fg: "46%", threePt: "38%" }, img: "https://a.espncdn.com/i/teamlogos/ncaa/500/9.png" },
+  { id: 53, rank: 53, name: "Amier Ali", school: "Arizona St", position: "SF", stats: { ppg: "10.8", rpg: "4.5", apg: "2.2", fg: "45%", threePt: "34%" }, img: "https://a.espncdn.com/i/teamlogos/ncaa/500/9.png" },
+  { id: 54, rank: 54, name: "Paul McNeil", school: "NC State", position: "SG", stats: { ppg: "11.5", rpg: "3.2", apg: "1.5", fg: "44%", threePt: "39%" }, img: "https://a.espncdn.com/i/teamlogos/ncaa/500/152.png" },
+  { id: 55, rank: 55, name: "Mercy Miller", school: "Houston", position: "SG", stats: { ppg: "12.1", rpg: "3.5", apg: "1.8", fg: "45%", threePt: "37%" }, img: "https://a.espncdn.com/i/teamlogos/ncaa/500/248.png" },
+  { id: 56, rank: 56, name: "Rocco Zikarsky", school: "Brisbane", position: "C", stats: { ppg: "8.5", rpg: "7.2", apg: "0.5", fg: "65%", threePt: "0%" }, img: "https://a.espncdn.com/i/teamlogos/ncaa/500/264.png" },
+  { id: 57, rank: 57, name: "Somto Cyril", school: "Georgia", position: "C", stats: { ppg: "7.8", rpg: "8.5", apg: "0.4", fg: "68%", threePt: "0%" }, img: "https://a.espncdn.com/i/teamlogos/ncaa/500/61.png" },
+  { id: 58, rank: 58, name: "Tyler Betsey", school: "Cincinnati", position: "SF", stats: { ppg: "10.2", rpg: "4.1", apg: "1.2", fg: "44%", threePt: "38%" }, img: "https://a.espncdn.com/i/teamlogos/ncaa/500/2132.png" },
+  { id: 59, rank: 59, name: "Chase McCarty", school: "Houston", position: "SF", stats: { ppg: "9.5", rpg: "3.5", apg: "1.0", fg: "43%", threePt: "36%" }, img: "https://a.espncdn.com/i/teamlogos/ncaa/500/248.png" },
+  { id: 60, rank: 60, name: "James Brown", school: "UNC", position: "C", stats: { ppg: "8.2", rpg: "6.5", apg: "0.8", fg: "61%", threePt: "0%" }, img: "https://a.espncdn.com/i/teamlogos/ncaa/500/153.png" },
+  { id: 61, rank: 61, name: "Pharaoh Compton", school: "SDSU", position: "PF", stats: { ppg: "9.1", rpg: "7.2", apg: "0.5", fg: "64%", threePt: "0%" }, img: "https://a.espncdn.com/i/teamlogos/ncaa/500/21.png" },
+];
