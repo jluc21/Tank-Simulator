b1d34bb6d763c80559a7d6d456019c1f86bece6c
jluc21 <164274427+jluc21@users.noreply.github.com>
2026-01-02 17:56:31 -0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    // This allows production builds to complete even if you have linting errors
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // This allows production builds to complete even if you have TS errors
     ignoreBuildErrors: true,
   },
 };
