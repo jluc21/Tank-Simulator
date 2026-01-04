d6ac64dec79d594400142a033c2fbf1fa85dc69a
jluc21 <164274427+jluc21@users.noreply.github.com>
2026-01-04 10:14:28 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    // This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // This allows production builds to successfully complete even if
+    // your project has TypeScript errors.
+    ignoreBuildErrors: true,
   },
 };
+
+module.exports = nextConfig;
